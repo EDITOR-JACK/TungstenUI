0f7a28eb9558217fa764539cf60602a4ea6534a7
EDITOR-JACK <jack@howridiculous.org>
2024-08-13 19:02:07 +0800

    LED set instead of update?

--- ui/tungsten/ramp-mode.c
+++ ui/tungsten/ramp-mode.c
@@ -23,9 +23,9 @@ uint8_t steady_state(Event event, uint16_t arg) {
     else if (event == EV_click1_release) {
         level_before_off = actual_level;
         if (overheatIndicator) {
-            rgb_led_update(0x20,0); //AUX LED Red High
+            rgb_led_set(0x20); //AUX LED Red High
         } else {
-            rgb_led_update(0x21,0); //AUX LED Orange High
+            rgb_led_set(0x21); //AUX LED Orange High
         }
         set_level_and_therm_target(0);
         return EVENT_HANDLED;
@@ -33,7 +33,7 @@ uint8_t steady_state(Event event, uint16_t arg) {
     // 2 clicks (early): abort turning off
     else if (event == EV_click2_press) {
         set_level_and_therm_target(level_before_off);
-        rgb_led_update(0x00,0); //AUX LED Off
+        rgb_led_set(0x00); //AUX LED Off
     }
     // 1 click: off
     else if (event == EV_1click) {
