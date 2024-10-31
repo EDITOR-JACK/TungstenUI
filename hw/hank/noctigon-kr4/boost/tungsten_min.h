31ccc52f58c049a6e5825b0ef680f24789cf5a42
EDITOR-JACK <jack@howridiculous.org>
2024-10-31 14:18:04 +0800

    Tungsten Min for KR4 Boost

---
+++ hw/hank/noctigon-kr4/boost/tungsten_min.h
@@ -0,0 +1,12 @@
+// Noctigon KR4 (12V) config options for Anduril
+// (and Noctigon KR1)
+#pragma once
+
+#include "hank/noctigon-dm11/boost/anduril.h"
+#include "hank/noctigon-dm11/boost/hwdef.h"
+#include "hank/anduril.h"
+
+#undef DEFAULT_THERM_CEIL
+#define DEFAULT_THERM_CEIL 60
+
+#undef USE_BUTTON_LED
