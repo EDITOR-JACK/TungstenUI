c03b6229b0642bf522e8b6974cef28bc74c6b672
EDITOR-JACK <jack@howridiculous.org>
2024-11-28 16:00:34 +0800

    Better test

--- ui/tungsten/ramp-mode.c
+++ ui/tungsten/ramp-mode.c
@@ -13,13 +13,13 @@ uint8_t steady_state(Event event, uint16_t arg) {
         return EVENT_HANDLED;
     }
 
-    // 1 click (early): OFF
+    /* 1 click (early): OFF
     else if (event == EV_click1_press) {
         set_level_and_therm_target(0);
         return EVENT_HANDLED;
     }
 
-    /* 1C: OFF
+    // 1C: OFF
     else if (event == EV_1click) {
         if (!ramp_now) {
             set_state(off_state, 0);
